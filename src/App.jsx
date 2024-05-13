028801aad67990159e3f0f506b0f0a3c74ca624b
Jashimuddin123 <adnanmahmud3394@gmail.com>
2024-05-13 16:04:31 +0600

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import Header from './components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
+    
+     <Header></Header>
 
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
     
     </>
   )
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+
+
+const Header = () => {
+    return (
+        <div>
+               <h1 className="text-3xl text-center text-green-600">Wellcome To Knowledge Cafe</h1>
+               <div className="flex justify-between mx-8">
+                <h2 className="text-4xl font-bold text-red-600">Knowledge Cafe</h2>
+                <img className="w-16 h-16 rounded-full" src="../../.././images/nav-logo.jpg" alt="" />
+               </div>
+        </div>
+    );
+};
+
+export default Header;
